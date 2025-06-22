d665d80df5b9ca2b777525cd7347422d4283002f
billie <billieartycode.studio@gmail.com>
2025-06-22 10:31:42 +0000

    script01

---
+++ scripts/cactus.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func cactus():
+	pass
+
+
+func _on_cactus_area_body_entered(body: Node2D) -> void:
+	if body.has_method("dino"):
+		get_tree().reload_current_scene()
+		#$timer.start()
+		
+
+func _on_timer_timeout() -> void:
+	pass
+	#get_tree().reload_current_scene()
